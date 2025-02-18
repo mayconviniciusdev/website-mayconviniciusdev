3a708fed78629e193423bfd509bd8819a4744005
Maycon Vinicius <contato@mayconviniciusdev.com>
2025-02-18 12:33:51 -0300

    feat: add button to navigate to contact form

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,15 @@
+export const ButtonBlue = () => {
+  return (
+    <div className="bg-blue hover:bg-blue/80 p-2 rounded-lg">
+      <button className="text-white">ENTRE EM CONTATO</button>
+    </div>
+  );
+}
+
+export const ButtonWhite = () => {
+  return (
+    <div className="bg-white hover:bg-white/80 py-4 rounded-lg text-center">
+      <button className="text-blue font-bold ">ENTRE EM CONTATO</button>
+    </div>
+  );
+}
