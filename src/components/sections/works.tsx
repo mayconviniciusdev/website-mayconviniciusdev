f23954c261e09b13423f901fa7d43f859c2b93f4
Maycon Vinicius <contato@mayconviniciusdev.com>
2026-01-20 20:43:31 -0300

    style: improve Works/Portfolio section visual layout

--- src/components/sections/works.tsx
+++ src/components/sections/works.tsx
@@ -1,40 +1,29 @@
-import Link from "next/link";
-
-import { SocialLinksBlue } from "../socialLinks/links";
-import { faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-
 import { Element } from 'react-scroll';
+import { LinksBlue } from "../socialLinks/links";
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 export default function Works() {
   return (
-    <section>
-    <Element name="works">
-        <div className="flex justify-center items-center mx-6 my-16">
-          <div>
-            <div className="flex justify-between mb-5">
-              <div className="flex items-center gap-x-2">
-                <h2 className="font-bold text-2xl text-blue">PORTFÓLIO |</h2>
-                <Link target="_blank" href={"https://github.com/mayconviniciusdev/"}>
-                  <p className="font-bold text-sm text-blue-cyan hover:text-blue-cyan/50">VER TODOS</p>
-                </Link>
-              </div>
-
-              <SocialLinksBlue
-              linkedinIcon={faLinkedin} 
-              instagramIcon={faInstagram}/>
+    <section className="px-4">
+      <Element name="works">
+        <div className="flex justify-center items-center my-16">
+          <div className="max-w-270">
+            <div className="flex justify-between items-center mb-5 md:flex-col md:items-start md:gap-2">
+              <h2 className="font-bold text-2xl text-[#004369]">PORTFÓLIO</h2>
+              <LinksBlue githubIcon={faGithub} linkedinIcon={faLinkedin} instagramIcon={faInstagram}/>
             </div>
 
-            <div className="max-w-[1440px]">
-              <div className="flex flex-col md:grid grid-cols-2 gap-5">
-                <div className="hover:opacity-80">
-                  <Link target="_blank" href={"/smartechrs"}>
-                    <img
-                      alt="Background"
-                      src="/smartechImage.png"
-                      className="rounded-tl-3xl object-cover"/>
-                  </Link>
-                </div>
-              </div>
+            <div className="grid md:grid-cols-2">
+              <Link target="_blank" href={"/smartechrs"}>
+                <img alt="" src="/smartechImage.png"
+                className="h-75 rounded-tl-3xl object-cover hover:opacity-80"/>
+              </Link>
+
+              <Link target="_blank" href={"/sectapera"}>
+                <img alt="" src="/sectaperaImage.png"
+                className="h-75 rounded-br-3xl object-cover hover:opacity-80 md:rounded-tr-3xl md:rounded-br-none"/>
+              </Link>
             </div>
           </div>
         </div>
