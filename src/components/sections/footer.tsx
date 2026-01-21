8f02f88c9c8f69973caada9edbd4702dea091230
Maycon Vinicius <contato@mayconviniciusdev.com>
2026-01-20 21:08:24 -0300

    style: improve Footer section visual layout

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,33 +1,26 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons"
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons"
-
-import { SocialLinksBlue } from "../socialLinks/links"
-
-import { Link } from "react-scroll"
+import { Link } from "react-scroll";
+import { LinksBlue } from "../socialLinks/links";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
   return (
-    <div className="mx-auto my-16 text-sm text-center">
-      <div className="mx-4 text-blue-cyan font-bold">
-        <Link 
-				className="flex items-center justify-center cursor-pointer text-xs" 
-				to="menu" offset={-55} smooth={true} duration={500}>
-					VOLTAR PARA CIMA
-					<FontAwesomeIcon 
-					icon={faArrowUp}
-					className="w-4"/>
+    <div className="mx-auto mt-10 mb-5 text-center">
+      <div className="px-4 text-[#004369]">
+        <Link to="menu" offset={-55} smooth={true} duration={500}
+				className="flex items-center justify-center cursor-pointer font-bold text-xs">
+					VOLTAR PARA CIMA <FontAwesomeIcon icon={faArrowUp} className="w-4"/>
 				</Link>
 
-				<p className="mt-4">RIO GRANDE DO SUL.</p>
-        <p>MAYCON VINÍCIUS, PROGRAMADOR FRONTEND.</p>
-        <span className="text-blue-cyan">contato@mayconviniciusdev.com</span>
+				<div className="font-semibold">
+					<p className="mt-4">RIO GRANDE DO SUL.</p>
+					<p>MAYCON VINÍCIUS, DESENVOLVEDOR WEB.</p>
+					<span className="text-[#0E7178]">contato@mayconviniciusdev.com</span>
+				</div>
 
-        <div className="mt-4">
-					<SocialLinksBlue 
-					githubIcon={faGithub}
-					linkedinIcon={faLinkedin}
-					instagramIcon={faInstagram}/>
+        <div className="mt-4 flex justify-center">
+					<LinksBlue githubIcon={faGithub} linkedinIcon={faLinkedin} instagramIcon={faInstagram}/>
 				</div>
 			</div>
     </div>
