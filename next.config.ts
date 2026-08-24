51147bc370b04b66690f548e7dd49555d0e1b5e8
Maycon Vinicius <contato@mayconviniciusdev.com>
2026-08-24 12:03:17 -0300

    fix: desativa otimização de imagem para exportação estática

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'export',
   trailingSlash: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
