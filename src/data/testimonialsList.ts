0e4ba421a36f5cf796f964353785c7edbd1e1780
Maycon Vinicius <contato@mayconviniciusdev.com>
2026-08-24 12:06:46 -0300

    feat(testimonialsList): adiciona novo depoimento e atualiza caminho da imagem

--- src/data/testimonialsList.ts
+++ src/data/testimonialsList.ts
@@ -12,8 +12,14 @@
 const testimonials = [
 	{
 		name: "Robbs Design",
-		img: "/robsonPhoto.png",
+		img: "/initial/photo-robson.png",
 		quote: `Sou muito grato por ter encontrado esse profissional pra fazer meu site da forma que imaginei. O bom gosto e dedicação que ele implantou no meu projeto não há dinheiro que pague.`,
+	},
+
+  {
+		name: "Iolanda Geschunder",
+		img: "/initial/photo-iolanda.svg",
+		quote: `Passando para agradecer pelo ótimo trabalho que prestou, o site ficou ótimo e super prático para os trabalhadores acessar, tivemos bons resultados! Obrigada e parabéns pelo trabalho de excelência.`,
 	},
 ];
 
